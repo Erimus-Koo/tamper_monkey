1a07769a4c32864239244773b1ec0630fff6e9a8
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2019-12-05 20:11:05 +0800

    + single click to stop scroll

--- auto_scroll.js
+++ auto_scroll.js
@@ -2,7 +2,7 @@
 // @name         Auto Scroll
 // @description  Auto Scroll Pages (double click / ctrl+arrow)
 // @include      *
-// @version      0.1
+// @version      0.11
 // @author       Erimus
 // @grant        none
 // @namespace    https://greasyfork.org/users/46393
@@ -20,10 +20,9 @@
     // main function
     let toggle_scroll = function(dire) {
         scrolling = !scrolling
-        if (dire != 'up') dire = 'down' // defualt direction
         if (scrolling) {
             console.log('Start scroll', dire)
-            dire = { 'up': -1, 'down': 1 } [dire]
+            dire = dire == 'up' ? -1 : 1
             auto_scroll = setInterval(function() {
                 document.documentElement.scrollTop += (dire * scroll_distance)
             }, scroll_interval)
@@ -36,6 +35,12 @@
     // toogle scrolling by double click
     document.body.addEventListener('dblclick', toggle_scroll)
 
+    // single click to stop scroll
+    document.body.addEventListener('click', function() {
+        console.log('Stop scroll')
+        clearInterval(auto_scroll)
+    })
+
     // toogle scrolling by hotkey
     // https://www.w3.org/2002/09/tests/keys.html
     document.onkeydown = function(e) {
@@ -44,8 +49,7 @@
         if (ctrlKey && keyCode == 40) {
             console.log('Press Ctrl + Down arrow')
             toggle_scroll()
-        }
-        else if (ctrlKey && keyCode == 38) {
+        } else if (ctrlKey && keyCode == 38) {
             console.log('Press Ctrl + Up arrow')
             toggle_scroll('up')
         }
