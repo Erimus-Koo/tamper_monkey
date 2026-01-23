fc5748bfaa5f3c6e4a1db7470af903af4a1cf2a1
Erimus <chuangqianmingyueguang+github@gmail.com>
2026-01-23 12:32:48 +0800

    feat: 增加确定性

--- qrcode_monkey_automation.js
+++ qrcode_monkey_automation.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QRCode Monkey Automation
 // @namespace    http://tampermonkey.net/
-// @version      2026-01-23
+// @version      0.0.2
 // @description  Auto paste url & set styles & generate
 // @author       Erimus
 // @match        https://www.qrcode-monkey.com/
@@ -12,7 +12,6 @@
 (function () {
   "use strict";
 
-  // 等待页面和相关DOM加载
   function waitForLabels(labels, callback) {
     const interval = setInterval(() => {
       let foundAll = labels.every((labelText) => findLabel(labelText));
@@ -23,7 +22,6 @@
     }, 500);
   }
 
-  // 根据label文本查找label DOM对象
   function findLabel(text) {
     let labels = document.querySelectorAll("label");
     for (let label of labels) {
@@ -34,11 +32,9 @@
     return null;
   }
 
-  // 执行点击操作
   function clickShape(labelText, shapeIndex) {
     let label = findLabel(labelText);
     if (!label) return;
-    // 找最近的兄弟.form-group.shape-group
     let group = label.nextElementSibling;
     while (
       group &&
@@ -48,7 +44,6 @@
       group = group.nextElementSibling;
     }
     if (!group) return;
-    // 寻找.shape元素
     let shapes = group.querySelectorAll(".shape");
     if (shapes.length >= shapeIndex) {
       shapes[shapeIndex - 1].click();
@@ -60,18 +55,16 @@
     clickShape("Eye Frame Shape", 12);
     clickShape("Eye Ball Shape", 13);
 
-    // 填入剪贴板内容到input#qrcodeUrl
     if (navigator.clipboard) {
       navigator.clipboard.readText().then((text) => {
         let input = document.querySelector("input#qrcodeUrl");
         if (input) {
           input.value = text;
-          // 点击生成二维码按钮
+          input.dispatchEvent(new Event("input", { bubbles: true }));
           document.querySelector("#button-create-qr-code")?.click();
         }
       });
     } else {
-      // 如果浏览器不支持Clipboard API，直接点击生成
       document.querySelector("#button-create-qr-code")?.click();
     }
   });
